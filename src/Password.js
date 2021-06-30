3ece6807b43023fd0f8d309ad6f3db1fb892c031
Matthew Wei <matthewwei35@gmail.com>
2021-06-30 16:45:12 -0700

    feat(password): format return string password

--- src/Password.js
+++ src/Password.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 
 function generatePassword() {
   let output = ''
-  for (let i = 0; i < 8; i += 1) {
+  for (let i = 0; i < 9; i += 1) {
     output += String.fromCharCode(Math.floor(Math.random() * 94) + 33);
   }
-  return output;
+  const beginning = output.slice(0, 3);
+  const middle = output.slice(3, 6);
+  const end = output.slice(6);
+  return `${beginning}-${middle}-${end}`
 }
 
 function Password() {
