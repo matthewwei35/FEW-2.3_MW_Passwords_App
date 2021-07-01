d06b10292e1b6b1d7774214cc919b299ecdd8320
Matthew Wei <matthewwei35@gmail.com>
2021-06-30 17:47:04 -0700

    feat(password): password and title displays in div

--- src/Password.js
+++ src/Password.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 
 function generatePassword() {
-  let output = ''
+  let output = '';
   for (let i = 0; i < 9; i += 1) {
     output += String.fromCharCode(Math.floor(Math.random() * 94) + 33);
   }
   const beginning = output.slice(0, 3);
   const middle = output.slice(3, 6);
   const end = output.slice(6);
-  return `${beginning}-${middle}-${end}`
+  return `${beginning}-${middle}-${end}`;
 }
 
 function Password() {
   const [password, setPassword] = useState('p@$$w0rd');
+  const [title, setTitle] = useState('');
  
   return (
     <div>
+      <div>{title}</div>
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        placeholder={'Title'}
+      />
       <div>{password}</div>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
       <div>
         <button onClick={(e) => {
           setPassword(generatePassword())
