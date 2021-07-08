a5fc79100e78064e1ffe429c86c92f10e7b609f3
Matthew Wei <matthewwei35@gmail.com>
2021-07-08 15:35:04 -0700

    feat(app): add passwordlist component to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Password from './Password';
+import PasswordList from './PasswordList';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
@@ -11,6 +12,7 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <Password />
+        <PasswordList />
       </div>
     </Provider>
   );
