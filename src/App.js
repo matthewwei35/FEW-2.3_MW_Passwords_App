5c4e6899a0c57c3d01bec182aba36554fbad751f
Matthew Wei <matthewwei35@gmail.com>
2021-07-11 11:13:29 -0700

    feat(app): add local storage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const PSSWRDZ_STATE = "PSSWRDZ_STATE";
+
+// Load State
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem(PSSWRDZ_STATE)
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+}
+
+// Save State
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(PSSWRDZ_STATE, serializedState)
+  } catch(err) {
+    console.log("Error saving data:"+err)
+  }
+}
+
+const persistedState = loadState();
+const store = createStore(reducers, persistedState);
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 function App() {
   return (
